0adccac9a120c74141199c267a55eb7ab9fae6e5
Alexey Malyshkin <amalyshkin00@gmail.com>
2023-06-22 00:39:06 -0500

    Lab 4

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,13 +60,33 @@ class MainActivity : ComponentActivity() {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        Greeting("Android")
-                        DataItemList(dataItems)
-                    }
+                    DataListScreen(dataItems)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DataListScreen(items: List<DataItem>) {
+    var dataItem by remember { mutableStateOf<DataItem?>(null) }
+    DataItemList(items) {dataItem = it }
+    dataItem?.let {
+        AlertDialog (
+            onDismissRequest = { dataItem = null },
+            title = { Text(it.name) },
+            text = { Text(it.description) },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        dataItem = null
+                    }
+                )
+                {
+                    Text("Okay")
+                }
+            }
+        )
     }
 }
 
@@ -69,6 +97,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+
 
 @Composable
 fun DataItemView(dataItem: DataItem) {
@@ -95,11 +125,18 @@ fun DataItemView(dataItem: DataItem) {
 }
 
 @Composable
-fun DataItemList(dataItems: List<DataItem>) {
+fun DataItemList(dataItems: List<DataItem>, dataItemClicked: (DataItem) -> Unit) {
     /* Create the list here. This function will call DataItemView() */
     LazyColumn {
         items(dataItems) { index ->
-            DataItemView(index)
+            Box (
+                modifier = Modifier.clickable {
+                    dataItemClicked(index)
+                }
+            )
+            {
+                DataItemView(index)
+            }
         }
     }
 }
